bc05629d4fe929dc801eb98a05769b9b228af0ed
Francois Richard <frichard@simexperts.com>
2016-03-25 13:15:00 -0400

    add axis

--- js/animated-bar-chart.js
+++ js/animated-bar-chart.js
@@ -5,7 +5,9 @@
       width = 800 - margin.left - margin.right,
       height = 600 - margin.top - margin.bottom;
 
-  var chart = d3.select(".chart")
+  var chart = d3.select(".chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -13,16 +15,24 @@
     .rangeRoundBands([0, width], 0.1);
 
   var y = d3.scale.linear()
-      .range([height, 0]);
+    .range([height, 0]);
 
   var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+    .scale(x)
+    .orient("bottom");
 
   var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .ticks(10, "%");
+    .scale(y)
+    .orient("left")
+    .ticks(10, "%");
 
+  chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
 
 })();
