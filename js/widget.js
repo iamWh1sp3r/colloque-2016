8bcc42f625b00e6c0f4c7abe4725e90ba9946646
François Richard <wh1sp3r@hotmail.com>
2016-04-20 09:59:10 -0400

    Widget refactoring

--- js/widget.js
+++ js/widget.js
@@ -1,44 +1,41 @@
 (function() {
   "use strict";
 
-  var widget = d3.selectAll(".widget")
-    .datum(function() {
-      var key = this.classList[1];
-      return DataService[key]().fetch();
-    });
-
   var delay = 300;
-  var height = widget.node().clientHeight;
+  var widgets = [
+    { label: "Nouveaux clients", klass: "customers", data: DataService.customers().fetch() },
+    { label: "Ventes mensuelles", klass: "sales", data: DataService.sales().fetch() },
+    { label: "Taux de satisfaction", klass: "rating", data: DataService.rating().fetch() }
+  ];
 
-  widget.append("h1").transition()
-    .duration(1000)
-    .delay(function(d, i) { return i * delay; })
+  var widget = d3.selectAll("#widgets").selectAll(".widget")
+      .data(widgets)
+    .enter().append("div")
+      .attr("class", function(d) { return ["widget", d.klass].join(" "); });
+
+  widget.append("h5")
+    .text(function(d) { return d.label; });
+
+  widget.append("h1")
+    .transition().duration(1000)
+      .delay(function(d, i) { return i * delay; })
       .tween("text", function(d) {
-        var i = d3.interpolate(0, d.value);
+        var i = d3.interpolate(0, d.data.value);
         return function(t) {
-          this.textContent = d.format(i(t));
+          this.textContent = d.data.format(i(t));
         };
       });
 
-  widget.insert("div", "h5")
-    .classed("target", true)
-    .style("height", 0)
-    .style("margin-top", height + "px")
-    .attr("title", function(d) {
-      return "Objectif: " + d.format(d.target);
-    })
-    .transition()
-    .duration(1000)
-    .delay(function(d, i) { return i * delay; })
-      .style("height", function(d) {
-        return ratingToHeight(d.value, d.target) + "px";
-      })
-      .style("margin-top", function(d) {
-        return (height - ratingToHeight(d.value, d.target)) + "px";
-      });
+  widget.append("div")
+      .attr("class", "target")
+      .style("width", 0)
+      .attr("title", function(d) { return "Objectif: " + d.data.format(d.data.target); })
+    .transition().duration(1000)
+      .delay(function(d, i) { return i * delay; })
+      .style("width", function(d) { return ratingPct(d.data.value, d.data.target) + "%"; });
 
-  function ratingToHeight(value, target) {
-    target = Math.max(value, target);
-    return (value * height / target);
+  function ratingPct(value, target) {
+    var max = Math.max(value, target);
+    return (value * 100 / max);
   }
 })();
